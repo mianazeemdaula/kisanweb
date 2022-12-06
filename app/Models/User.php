58306f46b92a19cae6c033323aa1251feed58780
mianazeemdaula <mianazeemdaula@gmail.com>
2022-12-06 23:05:29 +0500

    packig

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,14 @@ class User extends Authenticatable
     {
         return $this->hasMany(SocailAccount::class);
     }
+
+    public function following()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'following_id');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'following_id', 'follower_id');
+    }
 }
---
+++ database/migrations/2022_12_06_175958_create_reactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('offer_id');
+            $table->integer('type')->unsigned()->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reactions');
+    }
+};
