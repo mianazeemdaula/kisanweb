b0ae74ff56cc6615c4e0489f8d8f4a905382f26a
mianazeemdaula <mianazeemdaula@gmail.com>
2022-12-27 19:02:57 +0500

    auth

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
             'mobile' => 'required',
             'firebase_uid' => 'required',
         ]);
-        $user = User::where('mobile', $request->mobile, 'firebase_uid', $request->firebase_uid)->first();
+        $user = User::where('mobile', $request->mobile)->where('firebase_uid', $request->firebase_uid)->first();
         // if (! $user || ! Hash::check($request->password, $user->password)) {
         if (! $user) {
             return response()->json(['email' => 'The provided credentials are incorrect.'], 204); 
