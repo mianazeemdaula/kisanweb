edb0ced921351d6514889450c0886ea9c9d53841
mianazeemdaula <mianazeemdaula@gmail.com>
2023-01-04 18:23:28 +0500

    reviewer

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -5,7 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Review extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user that owns the Review
+     *
+     * @return \
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function reviewer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'review_by');
+    }
 }
