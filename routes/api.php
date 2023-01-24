9c7d4f79365cddc680468603aea574b8020c4e18
mianazeemdaula <mianazeemdaula@gmail.com>
2023-01-24 19:15:35 +0500

    get

--- routes/api.php
+++ routes/api.php
@@ -43,7 +43,7 @@ Route::resource('crop.type', CropTypeController::class);
 
 Route::resource('media', MediaController::class);
 
-Route::post('wa-message', [HomeController::class,'wamessage']);
+Route::get('wa-message', [HomeController::class,'wamessage']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     
