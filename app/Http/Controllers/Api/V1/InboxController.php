7df0c876f65a695f5779123e2d97a960ffc650de
mianazeemdaula <mianazeemdaula@gmail.com>
2023-01-26 11:07:28 +0500

    chat_id

--- app/Http/Controllers/Api/V1/InboxController.php
+++ app/Http/Controllers/Api/V1/InboxController.php
@@ -44,7 +44,7 @@ class InboxController extends Controller
         $data = Chat::with(['deal','buyer','lastmsg'])
         ->join('messages','messages.chat_id','chats.id')
         ->orderBy('messages.created_at','desc')
-        ->groupBy('chats.id')
+        ->groupBy('messages.chat_id')
         ->where('deal_id',$id)->paginate();
         return response()->json($data, 200);
     }
