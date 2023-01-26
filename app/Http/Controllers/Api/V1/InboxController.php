35a90f36a869b22415214f6c0bb1de9ac0b39068
mianazeemdaula <mianazeemdaula@gmail.com>
2023-01-26 18:13:54 +0500

    push

--- app/Http/Controllers/Api/V1/InboxController.php
+++ app/Http/Controllers/Api/V1/InboxController.php
@@ -41,17 +41,19 @@ class InboxController extends Controller
 
     public function show($id)
     {
-        $data = Chat::with(['deal','buyer','lastmsg'])->where('deal_id',$id)->paginate();
-        return response()->json($data, 200);
-
-        // $data = Chat::with(['deal','buyer','lastmsg'])
-        // ->leftJoin('messages', function($join) { 
-        //     $join->on('messages.chat_id', '=', 'chats.id')
-        //     ->on('messages.id', '=', \DB::raw("(SELECT max(id) from messages WHERE messages.chat_id = chats.id)"));
-        // })
-        // ->orderBy('messages.created_at','desc')
-        // ->where('chats.deal_id',$id)->paginate();
+        // $data = Chat::with(['deal','buyer','lastmsg'])->where('deal_id',$id)->paginate();
         // return response()->json($data, 200);
+
+        $data = Chat::with(['deal','buyer','lastmsg'])
+        ->leftJoin('messages', function($join) { 
+            $join->on('messages.chat_id', '=', 'chats.id')
+            ->on('messages.id', '=', \DB::raw("(SELECT max(id) from messages WHERE messages.chat_id = chats.id)"));
+        })
+        ->orderBy('messages.created_at','desc')
+        ->select('chats.*')
+        ->where('chats.deal_id',$id)->paginate();
+        //select * from ( SELECT chat_id, max(id) as latest FROM `messages` group by chat_id ) as t order by latest;
+        return response()->json($data, 200);
     }
 
     public function getChat($id)
