a68b591ef0bd62517b808a758d806f5ff95f4352
mianazeemdaula <mianazeemdaula@gmail.com>
2023-02-20 18:54:24 +0500

    city_id

--- app/Models/CropRate.php
+++ app/Models/CropRate.php
@@ -16,6 +16,7 @@ class CropRate extends Model
     protected $casts = [
         'min_price' => 'double',
         'max_price' => 'double',
+        'city_id' => 'integer',
         'crop_type_id' => 'integer'
     ];
 
