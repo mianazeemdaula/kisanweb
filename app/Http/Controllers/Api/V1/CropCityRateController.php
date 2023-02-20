bc9268527e02f02115e3e7121110dd9a180c32ed
mianazeemdaula <mianazeemdaula@gmail.com>
2023-02-20 20:33:42 +0500

    pag

--- app/Http/Controllers/Api/V1/CropCityRateController.php
+++ app/Http/Controllers/Api/V1/CropCityRateController.php
@@ -95,6 +95,7 @@ class CropCityRateController extends Controller
         $data = collect($paginate->items());
         $data->each->append('min_price_last','max_price_last');
         $paginate->setCollection($data);
+        return response()->json($paginate, 200, []);
     }
     
 }
