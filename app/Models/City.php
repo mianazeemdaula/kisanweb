362f1abc7a4d7db4ebe02f8b44b57d44c3308865
mianazeemdaula <mianazeemdaula@gmail.com>
2023-02-20 20:17:07 +0500

    rate

--- app/Models/City.php
+++ app/Models/City.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class City extends Model
 {
@@ -14,5 +15,10 @@ class City extends Model
     public function district(): BelongsTo
     {
         return $this->belongsTo(District::class);
+    }
+
+    public function rate(): HasOne
+    {
+        return $this->hasOne(CropRate::class);
     }
 }
--- app/Models/CropRate.php
+++ app/Models/CropRate.php
@@ -44,6 +44,7 @@ class CropRate extends Model
             'crop_type_id', 'city_id',
             \DB::raw('cast(avg(min_price) as float) as min_rate'),
             \DB::raw('cast(avg(max_price) as float) as max_rate'),
+            \DB::raw('max(rate_date) as rate_date'),
         )->groupBy('crop_type_id', 'city_id');
     }
 
