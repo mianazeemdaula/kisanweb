845ac426308cfb89f80985ca5f21815349d6ed37
mianazeemdaula <mianazeemdaula@gmail.com>
2023-02-22 13:34:58 +0500

    city

--- app/Http/Controllers/Api/V1/CityController.php
+++ app/Http/Controllers/Api/V1/CityController.php
@@ -12,9 +12,7 @@ class CityController extends Controller
 {
     public function index()
     {
-        $data = City::whereHas('district', function($q){
-            $q->whereIn('province_id', [4,6,7]);
-        })->orderBy('name')->select(['id','name'])->get();
+        $data = City::orderBy('name')->select(['id','name'])->get();
         return response()->json($data, 200);
     }
 }
