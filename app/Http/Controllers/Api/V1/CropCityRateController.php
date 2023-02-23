f250624ef79b5ab960d2c963cd424cdce3cd238b
mianazeemdaula <mianazeemdaula@gmail.com>
2023-02-23 18:46:08 +0500

    city

--- app/Http/Controllers/Api/V1/CropCityRateController.php
+++ app/Http/Controllers/Api/V1/CropCityRateController.php
@@ -83,7 +83,7 @@ class CropCityRateController extends Controller
 
     public function cityHistory(Request $request)
     {
-        $paginate =  CropRate::scopeRate()->where('crop_type_id', $request->crop)
+        $paginate =  CropRate::rate()->where('crop_type_id', $request->crop)
         ->where('city_id', $request->city)
         ->orderBy('rate_date','desc')
         ->paginate();
