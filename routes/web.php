50e72b79e01ba4578ac4831f4e91f8916e9fc79a
mianazeemdaula <mianazeemdaula@gmail.com>
2023-04-07 11:55:21 +0500

    s

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/statistics', function(){
     $data['last_day'] = \App\Models\User::whereHas('addresses')->whereDate('created_at',now()->subDays(1))->count();
     $data['deals'] = \App\Models\Deal::where('status','open')->count();
     $today = \App\Models\User::whereHas('address')->whereDate('created_at',now())->with('address')
-    // ->select('name','email', 'cnic','mobile')
+    ->select('name','email', 'cnic','mobile')
     ->get();
     $data['today_count'] = $today->count();
     $data['today'] = $today;
