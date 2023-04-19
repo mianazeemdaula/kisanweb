f77e647ca67a68dbf3dad5c7f611e6af17a202a5
mianazeemdaula <mianazeemdaula@gmail.com>
2023-04-19 12:01:22 +0500

    city

--- app/Http/Controllers/Api/V1/CropCityRateController.php
+++ app/Http/Controllers/Api/V1/CropCityRateController.php
@@ -10,6 +10,7 @@ use Carbon\Carbon;
 use App\Models\Crop;
 use App\Models\CropRate;
 use App\Models\CropType;
+use Illuminate\Support\Facades\DB;
 
 class CropCityRateController extends Controller
 {
@@ -56,17 +57,49 @@ class CropCityRateController extends Controller
 
     public function show($id)
     {
-        $paginate =  CropRate::cityRate()->with(['city'])->where('crop_type_id', $id)
-        ->leftJoin('cities', function($join) { 
-            $join->on('cities.id', '=', 'crop_rates.city_id');
-        })
-        ->orderBy('cities.name','asc')
-        ->orderBy('rate_date','desc')
-        ->paginate();
-        $data = collect($paginate->items());
-        $data->each->append('min_city_price_last','max_city_price_last');
-        $paginate->setCollection($data);
-        return response()->json($paginate, 200, []);
+        // $paginate =  CropRate::cityRate()->with(['city'])->where('crop_type_id', $id)
+        // ->leftJoin('cities', function($join) { 
+        //     $join->on('cities.id', '=', 'crop_rates.city_id');
+        // })
+        // ->orderBy('cities.name','asc')
+        // ->orderBy('rate_date','desc')
+        // ->paginate();
+        // $data = collect($paginate->items());
+        // $data->each->append('min_city_price_last','max_city_price_last');
+        // $paginate->setCollection($data);
+        // return response()->json($paginate, 200, []);
+
+        // $rates = DB::table('crop_rates as cr')
+        //     ->select(
+        //         'cr.city_id',
+        //         'cr.crop_type_id',
+        //         DB::raw('AVG(cr.min_price) AS min_rate'),
+        //         DB::raw('AVG(cr.max_price) AS max_rate'),
+        //         'cr.rate_date',
+        //         DB::raw('AVG(prev_cr.min_price) AS min_city_price_last'),
+        //         DB::raw('AVG(prev_cr.max_price) AS max_city_price_last'),
+        //     )
+        //     ->leftJoin('crop_rates as prev_cr', function($join) {
+        //         $join->on('cr.crop_type_id', '=', 'prev_cr.crop_type_id');
+        //         $join->on('prev_cr.rate_date', '=', DB::raw('(
+        //             SELECT MAX(rate_date) FROM crop_rates
+        //             WHERE crop_type_id = cr.crop_type_id AND rate_date < cr.rate_date
+        //         )'));
+        //     })
+        //     ->join('cities', 'cities.id', '=', 'cr.city_id')
+        //     // ->whereIn('cr.city_id', $id)
+        //     ->where('cr.crop_type_id',$id)
+        //     ->groupBy('cr.city_id', 'cr.crop_type_id', 'cr.rate_date')
+        //     ->orderBy('cr.rate_date', 'desc')
+        //     ->paginate();
+
+            $rates  = CropRate::cityWiseRate()
+            ->with(['city'])
+            ->where('cr.crop_type_id',$id)
+            ->orderBy('cities.name','asc')
+            ->paginate();
+
+            return response()->json($rates, 200,[]);
     }
 
     public function edit($id)
--- app/Models/CropRate.php
+++ app/Models/CropRate.php
@@ -138,4 +138,31 @@ class CropRate extends Model
         
         return $query->get();
     }
+
+    public function scopeCityWiseRate($query)
+    {
+        return $query->from('crop_rates as cr')->select(
+            'cr.city_id',
+            'cr.crop_type_id',
+            DB::raw('MIN(cr.min_price) AS min_rate'),
+            DB::raw('MAX(cr.max_price) AS max_rate'),
+            DB::raw('MAX(cr.rate_date) AS rate_date'),
+            DB::raw('MIN(prev_cr.min_price) AS min_city_price_last'),
+            DB::raw('MAX(prev_cr.max_price) AS max_city_price_last'),
+        )
+        ->leftJoin('crop_rates as prev_cr', function($join) {
+            $join->on('cr.crop_type_id', '=', 'prev_cr.crop_type_id');
+            $join->on('prev_cr.rate_date', '=', DB::raw('(
+                SELECT MAX(rate_date) FROM crop_rates
+                WHERE crop_type_id = cr.crop_type_id AND rate_date < cr.rate_date AND city_id = cr.city_id
+            )'));
+        })->whereIn('cr.id', function($query) {
+            $query->select(DB::raw('MAX(id)'))
+                ->from('crop_rates')
+                ->groupBy('city_id','crop_type_id');
+        })
+        ->join('cities', 'cities.id', '=', 'cr.city_id')
+        ->groupBy('cr.city_id', 'cr.crop_type_id', 'cr.rate_date')
+        ->orderBy('cr.rate_date', 'desc');
+    }
 }
