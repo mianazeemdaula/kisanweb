022984c2c5c08fb21f5079f5756dad9cd1a6cc7f
mianazeemdaula <mianazeemdaula@gmail.com>
2023-04-26 17:22:50 +0500

    repeat

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use Illuminate\Database\Events\QueryExecuted;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Database\Events\QueryExecuted;
+// use Illuminate\Support\Facades\File;
+// use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,11 +27,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        DB::listen(function(QueryExecuted $query) {
-            File::append(
-                storage_path('/logs/query.log'),
-                $query->sql . ' [' . implode(', ', $query->bindings) . ']' . '[' . $query->time . ']' . PHP_EOL
-           );
-        });
+        // DB::listen(function(QueryExecuted $query) {
+        //     File::append(
+        //         storage_path('/logs/query.log'),
+        //         $query->sql . ' [' . implode(', ', $query->bindings) . ']' . '[' . $query->time . ']' . PHP_EOL
+        //    );
+        // });
     }
 }
