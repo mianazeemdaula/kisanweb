de12b038b88aee59825e558840c9d8ee721cf28c
mianazeemdaula <mianazeemdaula@gmail.com>
2023-06-02 19:38:33 +0500

    kb

--- resources/views/components/stat-card.blade.php
+++ resources/views/components/stat-card.blade.php
@@ -1,4 +1,4 @@
-<div class="flex shadow-lg p-6 rounded-xl w-30">
+<div class="flex shadow-lg p-6 rounded-xl w-30 bg-white">
     <div class="{{ $color }} p-2 rounded-lg w-12 h-12 flex justify-center items-center">
         {{ $slot }}
     </div>
