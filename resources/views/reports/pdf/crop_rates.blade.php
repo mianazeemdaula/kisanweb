634a7d7c2de55fda1df45f66b0a5ff7628585c09
mianazeemdaula <mianazeemdaula@gmail.com>
2023-08-22 19:44:42 +0500

    s

--- resources/views/reports/pdf/crop_rates.blade.php
+++ resources/views/reports/pdf/crop_rates.blade.php
@@ -71,9 +71,9 @@
                                 <td class='text-center border border-gray-400 text-lg w-24'>{{ $rate->max_price }}</td>
                                 <td class='text-center border border-gray-400 text-lg w-10'>
                                     @if ($rate->min_price + $rate->max_price > $rate->min_price_last + $rate->max_price_last)
-                                        <i class="bi bi-arrow-up-short"></i>
+                                        <i class="bi bi-arrow-up-short bg-green-500"></i>
                                     @elseif ($rate->min_price + $rate->max_price < $rate->min_price_last + $rate->max_price_last)
-                                        <i class="bi bi-arrow-down-short"></i>
+                                        <i class="bi bi-arrow-down-short bg-red-500"></i>
                                     @else
                                         {{ '-' }}
                                     @endif
