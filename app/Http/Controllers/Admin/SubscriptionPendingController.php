2ded9f9dbe338bdde018cf4ff0a36a84dda3ccae
mianazeemdaula <mianazeemdaula@gmail.com>
2023-11-29 10:58:43 +0500

    as

--- app/Http/Controllers/Admin/SubscriptionPendingController.php
+++ app/Http/Controllers/Admin/SubscriptionPendingController.php
@@ -60,6 +60,11 @@ class SubscriptionPendingController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'status' => 'required|in:accept,reject',
+        ]);
+        return [$request->all(),$id];
         $package = SubscriptionPackage::findOrFail($id);
         $lastDate = now();
         if($package->duration_unit == 'month'){
@@ -69,27 +74,25 @@ class SubscriptionPendingController extends Controller
         }else{
             $lastDate = $lastDate->addDays($package->duration);
         }
-
         $active = $request->status == 'accept';
-
-        $package->users()->updateExistingPivot($request->user_id, [
+        User::find($request->user_id)->subscriptions()->updateExistingPivot($package->id, [
             'active' => $active,
             'start_date' => now(),
             'end_date' => $lastDate,
         ]);
 
         $data = $package->users()->wherePivot('user_id', $request->user_id)->first();
-        $waapi = new WaAPI();
-        $to = $request->contact;
-        if(Str::startsWith($to, '03')){
-            $to = '92'.substr($to, 1);
-        }
-        $to = $to."@c.us";
-        if($data->pivot->active){
-            $waapi->addGroupParticipant("120363168242340048@g.us",$to);
-        }else{
-            $waapi->removeGroupParticipant("120363168242340048@g.us", $to);
-        }
+        // $waapi = new WaAPI();
+        // $to = $request->contact;
+        // if(Str::startsWith($to, '03')){
+        //     $to = '92'.substr($to, 1);
+        // }
+        // $to = $to."@c.us";
+        // if($data->pivot->active){
+        //     $waapi->addGroupParticipant("120363168242340048@g.us",$to);
+        // }else{
+        //     $waapi->removeGroupParticipant("120363168242340048@g.us", $to);
+        // }
         return redirect()->route('admin.pending-subscriptions.index')->with('success', 'Subscription approved successfully.');
     }
 
