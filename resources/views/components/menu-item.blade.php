76a2197ce47c62c69a9224a20f2b8e1fcded2e76
mianazeemdaula <mianazeemdaula@gmail.com>
2026-07-16 12:56:55 +0500

    menufix

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,10 +1,10 @@
 @php
     $isActive = request()->url() === $url || request()->is(trim(parse_url($url, PHP_URL_PATH), '/'));
 @endphp
-<a href="{{ $url }}">
+<a href="{{ $url }}" class="block">
     <div
-        class="p-1.5 my-0.5 flex items-center rounded-lg px-3 duration-300 cursor-pointer hover:bg-emerald-500/10 hover:text-emerald-400 text-slate-450 {{ $isActive ? 'bg-emerald-500/10 text-emerald-400 font-semibold' : '' }} transition-all">
-        <i class="bi {{ $icon }} text-sm"></i>
-        <span class="text-xs ml-2.5">{{ $title }}</span>
+        class="p-2.5 my-1 flex items-center rounded-xl px-4 duration-300 cursor-pointer hover:bg-slate-800 hover:text-emerald-400 text-slate-400 {{ $isActive ? 'bg-slate-800 text-emerald-400 font-semibold shadow-sm' : '' }} transition-all">
+        <i class="bi {{ $icon }} text-base"></i>
+        <span class="text-sm ml-3">{{ $title }}</span>
     </div>
 </a>
